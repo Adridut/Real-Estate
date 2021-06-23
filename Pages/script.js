bae40c12e42e75c1454a25904ab3edf78558064e
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2021-06-23 16:33:52 +0200

    extracted css and js

---
+++ Pages/script.js
@@ -0,0 +1,31 @@
+let imageContainer = document.getElementById("imageContainer");
+for (let i = 0; i < 10; i++) {
+    imageContainer.innerHTML += ('<img src="../Assets/Images/home.jpg" style="max-width: 25%; margin: 10px;" alt="home"></img>')
+}
+
+let swiperContainer = document.getElementById("swiperContainer");
+for (let i = 0; i < 10; i++) {
+    swiperContainer.innerHTML += ('<img class="swiper-slide" src="../Assets/Images/home.jpg" alt="home"></img>')
+}
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
