2977a37c9cf838a97b95c3d51654d86de5dbc612
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2021-06-23 18:11:38 +0200

    added team

--- Pages/Properties/script.js
+++ Pages/Properties/script.js
@@ -1,11 +1,11 @@
 let imageContainer = document.getElementById("imageContainer");
-for (let i = 0; i < 10; i++) {
-    imageContainer.innerHTML += ('<img src="../Assets/Images/home.jpg" style="max-width: 25%; margin: 10px;" alt="home"></img>')
+for (let i = 0; i < 25; i++) {
+    imageContainer.innerHTML += ('<img src="../../Assets/Images/home.jpg" style="max-width: 25%; margin: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" alt="home"></img>')
 }
 
 let swiperContainer = document.getElementById("swiperContainer");
 for (let i = 0; i < 10; i++) {
-    swiperContainer.innerHTML += ('<img class="swiper-slide" src="../Assets/Images/home.jpg" alt="home"></img>')
+    swiperContainer.innerHTML += ('<img class="swiper-slide" src="../../Assets/Images/home.jpg" alt="home"></img>')
 }
 
 const swiper = new Swiper('.swiper-container', {
