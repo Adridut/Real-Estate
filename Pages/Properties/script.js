25a49bf674a5c8c196dbef800a272ab9cd5620fa
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2021-06-24 17:16:32 +0200

    popup

--- Pages/Properties/script.js
+++ Pages/Properties/script.js
@@ -1,12 +1,13 @@
 let imageContainer = document.getElementById("imageContainer");
 for (let i = 0; i < 9; i++) {
-    imageContainer.innerHTML += ('<img src="../../Assets/Images/home.jpg" style="width: 25%; margin: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" alt="home"></img>')
+    imageContainer.innerHTML += ('<a href="#popup" rel="modal:open" style="width: 25%; margin: 10px;"><img src="../../Assets/Images/home.jpg" style="width: 100%; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" alt="home"></img></a>')
 }
 
 let swiperContainer = document.getElementById("swiperContainer");
 for (let i = 0; i < 10; i++) {
     swiperContainer.innerHTML += ('<img class="swiper-slide" src="../../Assets/Images/home.jpg" alt="home"></img>')
 }
+
 
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
