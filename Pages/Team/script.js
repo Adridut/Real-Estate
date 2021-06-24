a68b75bc7af24e49d1c3d1346e669b1b23051c1d
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2021-06-24 15:47:23 +0200

    property and agent details

--- Pages/Team/script.js
+++ Pages/Team/script.js
@@ -1,5 +1,5 @@
 let imageContainer = document.getElementById("imageContainer");
-for (let i = 0; i < 25; i++) {
+for (let i = 0; i < 24; i++) {
     imageContainer.innerHTML += ('<div style="display: flex; margin: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); align-items: center;"><div style="width: 50%;"><img src="../../Assets/Images/team.jpg" style="width: 100%; margin: 10px;" alt="home"></img></div><div style="text-align: center; width: 50%;"><h4 style="color: #ffa530;">Kelian Anderson</h4></div></div>')
 }
 
