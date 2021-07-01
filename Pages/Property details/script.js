6d7bc3f80efc22af7e0776244d20688278fcabc3
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2021-07-01 17:48:30 +0200

    slider

--- Pages/Property details/script.js
+++ Pages/Property details/script.js
@@ -1,27 +1,17 @@
 
-let swiperContainer = document.getElementById("swiperContainer");
-for (let i = 0; i < 10; i++) {
-    swiperContainer.innerHTML += ('<img class="swiper-slide" src="../../Assets/Images/home.jpg" alt="home"></img>')
-}
+// let swiperContainer = document.getElementById("swiperContainer");
+// for (let i = 0; i < 9; i++) {
+//     swiperContainer.innerHTML += ('<img class="swiper-slide" src="../../Assets/Images/home.jpg" alt="home"></img>')
+// }
 
 const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
 
     // Navigation arrows
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 2,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
     },
 });
