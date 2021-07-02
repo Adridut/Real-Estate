07cb17c1d06b209d03cc2926e8741b7204d22e79
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2021-07-02 16:47:01 +0200

    property details

--- Pages/Property details/script.js
+++ Pages/Property details/script.js
@@ -1,9 +1,4 @@
 
-// let swiperContainer = document.getElementById("swiperContainer");
-// for (let i = 0; i < 9; i++) {
-//     swiperContainer.innerHTML += ('<img class="swiper-slide" src="../../Assets/Images/home.jpg" alt="home"></img>')
-// }
-
 const swiper = new Swiper('.swiper-container', {
 
     // Navigation arrows
