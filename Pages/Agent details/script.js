dd55dd006a6ef111bb9dafbfb0c5507cae96f6e4
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2021-07-02 18:17:02 +0200

    swiper for properties in  agent details

--- Pages/Agent details/script.js
+++ Pages/Agent details/script.js
@@ -1,10 +1,9 @@
 
-
 const swiper = new Swiper('.swiper-container', {
 
   loop: true,
-  slidesPerView: 2,
-  spaceBetween: 2,
+  slidesPerView: 4,
+  spaceBetween: 1,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
