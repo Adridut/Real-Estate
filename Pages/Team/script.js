719785bc83f67dddc27de9b3ad37d526e452c915
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2021-07-05 11:21:37 +0200

    agents list

--- Pages/Team/script.js
+++ Pages/Team/script.js
@@ -1,5 +1,5 @@
 let imageContainer = document.getElementById("imageContainer");
 for (let i = 0; i < 8; i++) {
-    imageContainer.innerHTML += ('<div class="cardContainer"><a href="../Agent details/index.html" class="link cardLink"><div class="cardImageContainer"><img src="../../Assets/Images/team.jpg" class="cardImage" alt="home"></img></div><div class="cardDescription"><h3>Kelian Anderson</h3><h4 class="title">Senior Property Seller</h4></p>example@gmail.com</p><p>+49 178 23342392</p><i class="fa fa-facebook icons"></i><i class="fa fa-linkedin icons"></i><i class="fa fa-twitter icons"></i></div></a></div>')
+    imageContainer.innerHTML += ('<div class="cardContainer"><a href="../Agent details/index.html" class="link cardLink"><div class="cardImageContainer"><img src="../../Assets/Images/team.jpg" class="cardImage" alt="home"></img></div><div class="cardDescription"><h3>Kelian Anderson</h3><h4 class="title">Senior Property Seller</h4><i class="fa fa-facebook icons"></i><i class="fa fa-linkedin icons"></i><i class="fa fa-twitter icons"></i></div></a></div>')
 }
 
